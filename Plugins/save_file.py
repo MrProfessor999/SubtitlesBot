84b37755c868b6be764f5325f0d6a74cb3674b7e
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2022-01-05 09:53:05 +0530

    Update save_file.py

--- Plugins/save_file.py
+++ Plugins/save_file.py
@@ -165,89 +165,23 @@ async def save_video(client, message):
             message_id = downloading.message_id
             )
 
+def searching(update: Update, context: CallbackContext):
+    if update.message.via_bot != None:
+        return
 
-@Client.on_message(filters.text & filters.regex('^http') & check_user)
-async def save_url(client, message):
+    search_message = context.bot.send_message(chat_id=update.effective_chat.id, text="Searching your subtitle file")
+    sub_name = update.effective_message.text
+    full_index, title, keyword = search_sub(sub_name)
+    inline_keyboard = []
+    if len(full_index) == 0:
+        context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=search_message.message_id, text="No results found")
+        return
+    
+    index = full_index[:15]
+    for i in index:
+        subtitle = title[i-1]
+        key = keyword[i-1]
+        inline_keyboard.append([InlineKeyboardButton(subtitle, callback_data=f"{key}")])
 
-    chat_id = message.from_user.id
-    save_filename = None
-
-    if "|" in message.text:
-        if len(message.text.split('|')) ==2 :
-            save_filename = message.text.split('|')[1].strip()
-            url = message.text.split('|')[0].strip()
-    else :
-        url = message.text.strip()
-
-    if save_filename and len(save_filename)>60:
-        return await client.sendMessage(chat_id, Chat.LONG_CUS_FILENAME)
-
-    r = requests.get(url, stream=True, allow_redirects=True)
-    if save_filename is None :
-        if 'content-disposition' in r.headers.keys() :
-            regx = 'filename="(.*?)"'
-            res = re.search(regx, str(r.headers))
-            if res :
-                save_filename = res.group(1)
-            else :
-                #removing argumets from url!
-                if '?' in url:
-                    url = ''.join(url.split('?')[0:-1])
-                save_filename = url.split('/')[-1]
-                save_filename = unquote(save_filename)
-        else :
-            if '?' in url:
-                url = ''.join(url.split('?')[0:-1])
-            save_filename = url.split('/')[-1]
-            save_filename = unquote(save_filename)
-
-    sent_msg = await client.send_message(chat_id, 'Preparing Your Download')
-    ext = save_filename.split('.')[-1]
-    if ext not in ['mp4','mkv'] :
-        return await sent_msg.edit(Chat.UNSUPPORTED_FORMAT.format(ext))
-
-    size = None
-    if 'content-length' in r.headers.keys() :
-        size = int(r.headers['content-length'])
-
-    if not size :
-        return await sent_msg.edit(Chat.FILE_SIZE_ERROR)
-    if size>(2*1000*1000*1000) :
-        return await sent_msg.edit(Chat.MAX_FILE_SIZE)
-
-    if not os.path.exists(Config.DOWNLOAD_DIR) :
-        os.mkdir(Config.DOWNLOAD_DIR)
-
-    current = 0
-    start = time.time()
-    filename = str(round(start))+'.'+ext
-
-    logging.info(url)
-
-    with requests.get(url, stream=True, allow_redirects=True) as r :
-        with open(os.path.join(Config.DOWNLOAD_DIR,filename), 'wb') as f :
-            for chunk in r.iter_content(chunk_size=1024*1024) :
-                if (chunk) :
-                    written = f.write(chunk)
-                    #current += 1024*1024
-                    current += written
-                    await progress_bar(current, size, 'Downloading Your File!', sent_msg, start)
-
-    logging.info(save_filename)
-
-    try:
-        await sent_msg.edit(
-            Chat.DOWNLOAD_SUCCESS.format(round(time.time()-start))
-            )
-    except:
-        pass
-
-    db.put_video(chat_id, filename, save_filename)
-    if db.check_sub(chat_id) :
-        text = Chat.CHOOSE_CMD
-    else :
-        text = 'Video File Downloaded.\nNow send Subtitle file!'
-    try:
-        await sent_msg.edit(text)
-    except:
-        pass
+    reply_markup = InlineKeyboardMarkup(inline_keyboard)
+    context.bot.edit_message_text(chat_id=update.effective_chat.id, message_id=search_message.message_id, text=f"Got the following results for your query *{sub_name}*. Select the preffered type from the below options", parse_mode="Markdown", reply_markup=reply_markup)
